22ae75f85950804fad3f940b967b1cedab57d782
Doğancan <dogancanors@gmail.com>
2022-03-30 20:49:04 +0300

    fourth commit

--- resources/views/calculate.blade.php
+++ resources/views/calculate.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Result</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -14,6 +15,7 @@
 
     <div class="container bg-light">
         <div class="text-center mt-5 fs-3">
+            @csrf
             <?php
             for($i = 0; $i < count($freq); $i++)
             {
